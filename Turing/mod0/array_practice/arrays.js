a22513095654655ef191231514a60cb2e1e0bfce
Timothy Pendarvis <trpendarvis@gmail.com>
2022-08-03 15:22:27 -0600

    Add 3 Array Methods

--- Turing/mod0/array_practice/arrays.js
+++ Turing/mod0/array_practice/arrays.js
@@ -1,11 +1,21 @@
-## Mod0 Arrays Practice
+// ## Mod0 Arrays Practice
 
 // Declare a variable that stores an Array of at least 4 Strings.
 // Declare a variable that stores an Array of at least 4 Numbers.
 // Declare a variable that stores an Array of at least 4 Booleans.
 
-var name = ["Tim", "Allye", "Martin", "Anothony"]
+var name = ["Tim", "Allye", "Martin", "Anthony"]
 var age = [31.5, 28, 32, 31]
 var boolean = [true, false, false, true]
 
-// 
+// Array methods
+name.pop();
+console.log(name);
+//Pop should remove the last element from the name Array, when excuted all names should be displayed besides
+// "Anthony".
+age.push();
+console.log(age);
+//Push will place any new element to the end of the Array when added.
+boolean.shift();
+console.log(boolean);
+//Shift will remove the first element in the Array.
