c99f01e1c500c66c8f101b70b9a1a349c9307de7
Timothy Pendarvis <trpendarvis@gmail.com>
2022-08-03 15:08:48 -0600

    Add 3 lines Strings, Numbers, Booleans

---
+++ Turing/mod0/array_practice/arrays.js
@@ -0,0 +1,11 @@
+## Mod0 Arrays Practice
+
+// Declare a variable that stores an Array of at least 4 Strings.
+// Declare a variable that stores an Array of at least 4 Numbers.
+// Declare a variable that stores an Array of at least 4 Booleans.
+
+var name = ["Tim", "Allye", "Martin", "Anothony"]
+var age = [31.5, 28, 32, 31]
+var boolean = [true, false, false, true]
+
+// 
